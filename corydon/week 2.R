d2be66befc756ae3bb2824c84efae46642451c14
Baylor, Corydon [USA] <Baylor_Corydon@bah.com>
2020-02-04 16:07:04 -0500

    completed a tds tutorial

--- corydon/week 2.R
+++ corydon/week 2.R
@@ -4,3 +4,69 @@ library(rvest)
 library(stringr)
 library(stringi)
 library(tidyverse)
+
+# Quick and Easy Medium Post on rvest ---------------------------
+
+# https://towardsdatascience.com/learn-to-create-your-own-datasets-web-scraping-in-r-f934a31748a5
+# The goal here is to rework this tutorial for the updated webpages. Basically the CSS classes
+# have changed names. So we have to refind them ---yeet!
+
+# Identify the url from where you want to extract data
+base_url <- "https://www.billboard.com/charts/artist-100"
+webpage <- read_html(base_url)
+
+# Get the artist name
+artist <- html_nodes(webpage, ".chart-list-item__title")
+artist <- as.character(html_text(artist))
+
+# Get the artist rank
+rank <- html_nodes(webpage, ".chart-list-item__rank")
+rank <- as.numeric(html_text(rank))
+
+# Save it to a tibble
+top_artists <- tibble('Artist' = gsub("\n", "", artist),   #remove the \n character in the artist's name
+                      'Rank' = rank) %>%
+  filter(rank <= 10)
+
+#Format the link to navigate to the artists genius webpage
+genius_urls <- paste0("https://genius.com/artists/",top_artists$Artist)
+
+# you may note that there are spaces in the urls
+genius_urls[1] # but when you put them in your web browser it finds them anywho
+
+#Initialize a tibble to store the results
+artist_lyrics <- tibble()
+
+# Outer loop to get the song links for each artist 
+for (i in 1:10) {
+  genius_page <- read_html(genius_urls[i])
+  song_links <- html_nodes(genius_page, ".mini_card_grid-song a") %>%
+    html_attr("href") 
+  #Inner loop to get the Song Name and Lyrics from the Song Link    
+  for (j in 1:3) {
+    
+    # Get lyrics
+    lyrics_scraped <- read_html(song_links[j]) %>%
+      html_nodes("div.lyrics p") %>%
+      html_text()
+    
+    # Get song name
+    song_name <- read_html(song_links[j]) %>%
+      html_nodes("h1.header_with_cover_art-primary_info-title") %>%
+      html_text()
+    
+    # Save the details to a tibble
+    artist_lyrics <- rbind(artist_lyrics, tibble(Rank = top_artists$Rank[i],
+                                                 Artist = top_artists$Artist[i],
+                                                 Song = song_name,
+                                                 Lyrics = lyrics_scraped ))
+    
+    print(i) # so you know where we are
+    # Insert a time lag - to prevent me from getting booted from the site :)
+    Sys.sleep(10)
+    
+  }
+} 
+
+#Inspect the results
+artist_lyrics
